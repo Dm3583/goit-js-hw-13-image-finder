e4514b9ce7e360b8d384f423330d544b4953548b
Dm3583 <pankovdm@gmail.com>
2021-01-05 16:15:22 -0800

    search for query

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
 import './styles.css';
-import apiService from './js/apiService';
-
-console.log(apiService.accessKey);
-
-fetch(`https://pixabay.com/api/?key=${apiService.accessKey}&q=dog`)
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(err => console.log(err));
+import './js/apiService';
+import './js/searchForQuery';
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,5 +1,20 @@
+const accessKey = '19762883-8865d0dea9f1f7e21a434f769';
+
+console.log(accessKey);
 
 export default {
-    accessKey: '19762883-8865d0dea9f1f7e21a434f769'
+
+    query: '',
+
+
+    fetchForQuery(query) {
+        return fetch(`https://pixabay.com/api/?key=${accessKey}&q=${query}`)
+            .then(res => res.json())
+            .then(data => console.log(data))
+            .catch(err => console.log(err));
+    },
+
+
+
 
 }
